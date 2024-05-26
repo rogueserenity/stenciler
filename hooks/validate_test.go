15208bbb636e438c011e67cdd82012ab6d881c48
Jay Burgess <jay@rogueserenity.com>
2024-05-26 15:47:30 +0000

    add hook validation tests

---
+++ hooks/validate_test.go
@@ -0,0 +1,110 @@
+package hooks_test
+
+import (
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/rogueserenity/stenciler/config"
+	"github.com/rogueserenity/stenciler/hooks"
+)
+
+var _ = Describe("Validate", func() {
+
+	DescribeTable("Validate with non-existent hooks", func(template *config.Template) {
+		repoPath := "test-repo"
+		err := hooks.Validate(template, repoPath)
+		Expect(err).To(MatchError("hook invalid-hook does not exist"))
+	},
+		Entry("ValidationHook", &config.Template{
+			Params: []config.Param{
+				{
+					ValidationHook: "invalid-hook",
+				},
+			},
+		}),
+		Entry("PreInitHookPaths", &config.Template{
+			PreInitHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PostInitHookPaths", &config.Template{
+			PostInitHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PreUpdateHookPaths", &config.Template{
+			PreUpdateHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PostUpdateHookPaths", &config.Template{
+			PostUpdateHookPaths: []string{"invalid-hook"},
+		}),
+	)
+
+	DescribeTable("Validate with non-executable hooks", func(template *config.Template) {
+		repoPath, err := os.MkdirTemp("", "test-repo")
+		Expect(err).NotTo(HaveOccurred())
+		defer os.RemoveAll(repoPath)
+		file, err := os.Create(filepath.Join(repoPath, "invalid-hook"))
+		Expect(err).NotTo(HaveOccurred())
+		err = file.Close()
+		Expect(err).NotTo(HaveOccurred())
+
+		err = hooks.Validate(template, repoPath)
+		Expect(err).To(MatchError("hook invalid-hook is not executable"))
+	},
+		Entry("ValidationHook", &config.Template{
+			Params: []config.Param{
+				{
+					ValidationHook: "invalid-hook",
+				},
+			},
+		}),
+		Entry("PreInitHookPaths", &config.Template{
+			PreInitHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PostInitHookPaths", &config.Template{
+			PostInitHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PreUpdateHookPaths", &config.Template{
+			PreUpdateHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PostUpdateHookPaths", &config.Template{
+			PostUpdateHookPaths: []string{"invalid-hook"},
+		}),
+	)
+
+	DescribeTable("Validate with valid hooks", func(template *config.Template) {
+		repoPath, err := os.MkdirTemp("", "test-repo")
+		Expect(err).NotTo(HaveOccurred())
+		defer os.RemoveAll(repoPath)
+		file, err := os.Create(filepath.Join(repoPath, "invalid-hook"))
+		Expect(err).NotTo(HaveOccurred())
+		err = file.Chmod(0755)
+		Expect(err).NotTo(HaveOccurred())
+		err = file.Close()
+		Expect(err).NotTo(HaveOccurred())
+
+		err = hooks.Validate(template, repoPath)
+		Expect(err).ToNot(HaveOccurred())
+	},
+		Entry("ValidationHook", &config.Template{
+			Params: []config.Param{
+				{
+					ValidationHook: "invalid-hook",
+				},
+			},
+		}),
+		Entry("PreInitHookPaths", &config.Template{
+			PreInitHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PostInitHookPaths", &config.Template{
+			PostInitHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PreUpdateHookPaths", &config.Template{
+			PreUpdateHookPaths: []string{"invalid-hook"},
+		}),
+		Entry("PostUpdateHookPaths", &config.Template{
+			PostUpdateHookPaths: []string{"invalid-hook"},
+		}),
+	)
+
+})
