419181e49aa12ab507a7045e9837992f41c4a322
Jay Burgess <jay@rogueserenity.com>
2024-05-28 00:04:36 +0000

    add todos

--- files/raw.go
+++ files/raw.go
@@ -4,5 +4,6 @@ import "github.com/rogueserenity/stenciler/config"
 
 // CopyRaw copies files that do not require template processing into the current working directory.
 func CopyRaw(repoDir string, template *config.Template) error {
+	// TODO: Implement this function
 	return nil
 }
--- files/template.go
+++ files/template.go
@@ -5,5 +5,6 @@ import "github.com/rogueserenity/stenciler/config"
 // CopyTemplated copies all templated files (those no located in raw-copy) and optionaly excluding files listed in
 // init-only by passing them through the template engine, then writing the copies into the current working directory.
 func CopyTemplated(repoDir string, template *config.Template, excludeInitOnly bool) error {
+	// TODO: Implement this function
 	return nil
 }
